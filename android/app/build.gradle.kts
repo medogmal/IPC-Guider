83b256dad488d0f2f9876067aa701e077dcbe2e0
medogmal <medogmal@users.noreply.github.com>
2025-12-07 22:40:56 +0200

    Android release prep: bump version to 1.1.0+2, targetSdk/compileSdk 34, fix build dir, keep signing files ignored

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.dryazeed.ipcguider"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -34,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
